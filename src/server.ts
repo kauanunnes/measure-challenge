0f868c526f6030c48e36f7ec56411ccb08223f3e
Kaua Nunes <kaua.nunes@vilt-group.com>
2024-08-27 09:47:26 -0300

    Creating a node server with Exress

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("starting server...")
+import dotenv from 'dotenv'
+import express, {Request, Response} from 'express'
+
+dotenv.config()
+
+const app = express()
+
+const port = 3434
+
+app.get('/', (req: Request, res: Response) => {
+   res.send("Started server...")
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http:localhost:${port}`)
+})
